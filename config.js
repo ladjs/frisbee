70dd5b8290898692e2cf0cd8841d033be6ea6d69
niftylettuce <niftylettuce@gmail.com>
2018-05-21 03:55:25 -0500

    Added docs

---
+++ config.js
@@ -0,0 +1,15 @@
+docute.init({
+  debug: true,
+  title: 'Frisbee',
+  repo: 'niftylettuce/frisbee',
+  'edit-link': 'https://github.com/niftylettuce/frisbee/tree/master/',
+  twitter: 'niftylettuce',
+  nav: {
+    default: [
+      {
+        title: 'Modern fetch-based alternative to axios/superagent/request. Great for React Native.',
+        path: '/'
+      }
+    ]
+  }
+});
